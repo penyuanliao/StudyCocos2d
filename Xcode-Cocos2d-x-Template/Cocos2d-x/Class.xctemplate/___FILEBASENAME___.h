48bd6e975e9adaae2a424fc63540bdaad529b9e4
Benson Liao <anaylst@gmail.com>
2015-06-10 13:58:51 +0800

    Xcode Add Template

---
+++ Xcode-Cocos2d-x-Template/Cocos2d-x/Class.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,25 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+
+#include "cocos2d.h"
+
+class ___FILEBASENAMEASIDENTIFIER___ : public cocos2d::Layer
+{
+private:
+    ___FILEBASENAMEASIDENTIFIER___();
+    virtual ~___FILEBASENAMEASIDENTIFIER___();
+public:
+    static cocos2d::Scene* createScene();
+    bool init();
+    CREATE_FUNC(___FILEBASENAMEASIDENTIFIER___);
+};
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____) */
---
+++ Xcode-Cocos2d-x-Template/runBuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -R ./cocos2d-x ~/Library/Developer/Xcode/Templates/File\ Templates/
+
+cd ~/Library/Developer/Xcode/Templates/File\ Templates/cocos2d-x/class.xctemplate
+
+ls -la
+
+echo 'done'
