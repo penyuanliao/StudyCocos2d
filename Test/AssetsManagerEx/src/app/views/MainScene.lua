1ae3529816fd42ff374cbae405bdafb3f97eba7b
penyuanliao <anaylst@gmail.com>
2015-06-24 09:06:03 +0800

    測試熱更新
    Cocos2d-x 3.6 版本使用AssetsManagerEx機制

---
+++ Test/AssetsManagerEx/src/app/views/MainScene.lua
@@ -0,0 +1,28 @@
+
+local MainScene = class("MainScene", cc.load("mvc").ViewBase)
+
+function MainScene:onCreate()
+    -- add background image
+    display.newSprite("MainSceneBg.jpg")
+        :move(display.center)
+        :addTo(self)
+
+    -- add play button
+    local playButton = cc.MenuItemImage:create("PlayButton.png", "PlayButton.png")
+        :onClicked(function()
+            self:getApp():enterScene("PlayScene")
+        end)
+    cc.Menu:create(playButton)
+        :move(display.cx, display.cy - 200)
+        :addTo(self)
+        
+        MainScene:test()
+        
+        
+end
+
+function MainScene:test()
+print("test")
+end
+
+return MainScene
